faddc869d32ceea7b08fd37499b451dfe87fd668
tomsdoo <32782986+tomsdoo@users.noreply.github.com>
2022-12-17 13:09:58 +0900

    test for GitHubRepo getTree() (#5)

---
+++ __test__/GitHubRepo/getTree.test.ts
@@ -0,0 +1,64 @@
+import { afterEach, beforeEach, describe, it, expect, jest } from "@jest/globals";
+import { GitHubRepo } from "@/GitHubRepo";
+
+const callstacks = {
+  octokit: {
+    getTree: [],
+  },
+};
+
+jest.mock("@octokit/rest", () => ({
+  Octokit: class Octokit {
+    protected token: string;
+    public rest: any;
+    constructor({ auth }: { auth: string; }){
+      this.token = auth;
+      this.rest = {
+        git: {
+          getTree: async ({ owner, repo, tree_sha, recursive  }: { owner: string; repo: string; tree_sha: string; recursive: string; }) => {
+            callstacks.octokit.getTree.push({ owner, repo, tree_sha, recursive });
+            return await Promise.resolve({
+              data: {
+                tree: [
+                  {
+                    path: "dummyPath1",
+                  },
+                ],
+              },
+            });
+          }
+        }
+      };
+    }
+  }
+}));
+
+describe("GitHugRepo", () => {
+  let githubToken: string;
+  let owner: string;
+  let repo: string;
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+  beforeEach(() => {
+    githubToken = "dummyGithubToken";
+    owner = "dummyOwner";
+    repo = "dummyRepo";
+  });
+  describe("getTree()", () => {
+    it("success", async () => {
+      const instance = new GitHubRepo(githubToken, owner, repo);
+      expect(await instance.getTree("dummySha")).toEqual([
+        {
+          path: "dummyPath1",
+        },
+      ]);
+      expect(callstacks.octokit.getTree.pop()).toStrictEqual({
+        owner: "dummyOwner",
+        repo: "dummyRepo",
+        tree_sha: "dummySha",
+        recursive: "true",
+      });
+    });
+  });
+});
