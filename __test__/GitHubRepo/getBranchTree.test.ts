6191436bacf19a1b25c4c41313b4ca4a65c83709
tom <toms@live.jp>
2023-09-03 06:10:43 +0900

    test for getBranchTree() is refactored

--- __test__/GitHubRepo/getBranchTree.test.ts
+++ __test__/GitHubRepo/getBranchTree.test.ts
@@ -7,40 +7,54 @@ import {
   jest,
 } from "@jest/globals";
 import { GitHubRepo } from "@/GitHubRepo";
+import { owner, repo, token } from "./constants";
 
 describe("GitHubRepo", () => {
-  let githubToken: string;
-  let owner: string;
-  let repo: string;
   afterEach(() => {
     jest.clearAllMocks();
   });
+  let mockedTree: Array<{
+    path?: string;
+    mode?: string;
+    type?: string;
+    sha?: string;
+    size?: number;
+    url?: string;
+  }>;
+  let githubRepo: GitHubRepo;
+  let spyGetBranchSha: jest.Spied<typeof GitHubRepo.prototype.getBranchSha>;
+  let spyGetTree: jest.Spied<typeof GitHubRepo.prototype.getTree>;
   beforeEach(() => {
-    githubToken = "dummyToken";
-    owner = "dummyOwner";
-    repo = "dummyRepo";
+    githubRepo = new GitHubRepo(token, owner, repo);
+    spyGetBranchSha = jest
+      .spyOn(GitHubRepo.prototype, "getBranchSha")
+      .mockResolvedValue("dummySha");
+    mockedTree = [
+      {
+        path: "dummyPath1",
+      },
+      {
+        path: "dummyPath2",
+      },
+    ];
+    spyGetTree = jest
+      .spyOn(GitHubRepo.prototype, "getTree")
+      .mockResolvedValue(mockedTree);
   });
-  describe("getBranchTree", () => {
-    it("success", async () => {
-      const spyGetBranchSha = jest
-        .spyOn(GitHubRepo.prototype, "getBranchSha")
-        .mockReturnValue(Promise.resolve("dummySha"));
-      const mockedValue = [
-        {
-          path: "dummyPath1",
-        },
-        {
-          path: "dummyPath2",
-        },
-      ];
-      const spyGetTree = jest
-        .spyOn(GitHubRepo.prototype, "getTree")
-        .mockReturnValue(Promise.resolve(mockedValue));
-      const instance = new GitHubRepo(githubToken, owner, repo);
-      expect(await instance.getBranchTree("dummyBranch")).toStrictEqual(
-        mockedValue
-      );
+  describe("getBranchTree()", () => {
+    it("resolved value is correct", async () => {
+      await expect(
+        githubRepo.getBranchTree("dummyBranch")
+      ).resolves.toStrictEqual(mockedTree);
+    });
+
+    it("calls getBranchSha()", async () => {
+      await githubRepo.getBranchTree("dummyBranch");
       expect(spyGetBranchSha).toHaveBeenCalledWith("dummyBranch");
+    });
+
+    it("calls getTree()", async () => {
+      await githubRepo.getBranchTree("dummyBranch");
       expect(spyGetTree).toHaveBeenCalledWith("dummySha");
     });
   });
