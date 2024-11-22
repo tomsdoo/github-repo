dfeacc99d7a19880371489d104c69b49eb7a8146
tomsdoo <32782986+tomsdoo@users.noreply.github.com>
2024-11-23 06:36:36 +0900

    PageLooper (#28)

---
+++ __test__/PageLooper/constructor.test.ts
@@ -0,0 +1,25 @@
+import { describe, it, expect } from "vitest";
+import { PageLooper } from "@/PageLooper";
+
+function regardAsHasPerPage(v: unknown): { perPage: number } {
+  return v as { perPage: number };
+}
+
+function regardAsHasPage(v: unknown): { page: number } {
+  return v as { page: number };
+}
+
+describe("PageLooper", () => {
+  describe("constructor", () => {
+    it("has perPage", () => {
+      expect(regardAsHasPerPage(new PageLooper(100))).toHaveProperty(
+        "perPage",
+        100,
+      );
+    });
+
+    it("has page", () => {
+      expect(regardAsHasPage(new PageLooper(100))).toHaveProperty("page", 1);
+    });
+  });
+});
