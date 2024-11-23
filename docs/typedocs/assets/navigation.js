3fc159fdf4a076cbf92df5225b3ee2e3f031f611
tomsdoo <32782986+tomsdoo@users.noreply.github.com>
2024-11-23 12:28:40 +0900

    npm update (#31)

--- docs/typedocs/assets/navigation.js
+++ docs/typedocs/assets/navigation.js
@@ -1,1 +1,1 @@
-window.navigationData = "data:application/octet-stream;base64,H4sIAAAAAAAAE4uuVipJrShRslJyzyzxKE0KSi3IV9JRKkgsyVCyUkrOSSwuTi3WR8jpZZTk5ijpKGVn5qUoWRkaWdTGAgCAi/gqQwAAAA=="
+window.navigationData = "data:application/octet-stream;base64,H4sIAAAAAAAAA4uuVipJrShRslJyzyzxKE0KSi3IV9JRKkgsyVCyUkrOSSwuTi3WR8jpZZTk5ijpKGVn5qUoWRkaWdTGAgCAi/gqQwAAAA=="
